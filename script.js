8450f9bb2c32fc1832553d21469f7736cef9c73c
liam.brady <liam.brady@wushustudios.com>
2024-12-05 13:30:57 +0000

    1st release

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const quizData = [
     { question: "Do Bohk",                      a:"Belt",                 b:"Uniform/Suit",                  c:"Sit",                     correct:"b"}, // 7
     { question: "Dee",                          a:"Uniform/suit",         b:"Belt",                          c:"Training hall or studio", correct:"b"}, // 8
     { question: "Sun Bae",                      a:"Senior member",        b:"Instructor",                    c:"Uniform/suit",            correct:"a"}, // 9
-    { question: "Sae Kye Tang Soo Do Hyup Hoi", a:"Association flag",     b:"World Tang Soo Do Association", c:"National flag",           correct:"c"}, // 10
+    { question: "Sae Kye Tang Soo Do Hyup Hoi", a:"Association flag",     b:"World Tang Soo Do Association", c:"National flag",           correct:"b"}, // 10
     { question: "Shim Sa Kwan Nim",             a:"Examiner",             b:"Grandmaster",                   c:"Instructor",              correct:"a"}, // 11
     { question: "Shim Sa",                      a:"Integrity",            b:"Testing (Gup and Dan)",         c:"Self defence",            correct:"b"}, // 12
     { question: "Hu Bae",                       a:"Black belt",           b:"Junior member",                 c:"Examiner",                correct:"b"}, // 13
@@ -28,35 +28,65 @@ const quizData = [
 
 const quiz = document.getElementById('quiz');
 const submitButton = document.getElementById('submit');
+const resetButton = document.getElementById('reset');
+resetButton.disabled = true
 const results = document.getElementById('results');
 
-let currentQuiz = 0;
-let score = 0;
-
-function loadQuiz() {
+function resetQuiz() {
+    submitButton.disabled = false;
+    results.innerHTML = ''
     quiz.innerHTML = '';
     quizData.forEach((currentQuestion, index) => {
         quiz.innerHTML += `
             <div class="question">
-                Question ${index + 1}: ${currentQuestion.question}
-                <label class="answer-options"><input type="radio" name="answer${index}" value="a"> ${currentQuestion.a}</label>
-                <label class="answer-options"><input type="radio" name="answer${index}" value="b"> ${currentQuestion.b}</label>
-                <label class="answer-options"><input type="radio" name="answer${index}" value="c"> ${currentQuestion.c}</label>
+                <label><strong>Question ${index + 1}: ${currentQuestion.question}</strong></label><br>
+                <label><input type="radio" name="answer${index}" value="a"> ${currentQuestion.a}</label><br>
+                <label><input type="radio" name="answer${index}" value="b"> ${currentQuestion.b}</label><br>
+                <label><input type="radio" name="answer${index}" value="c"> ${currentQuestion.c}</label>
             </div>
         `;
     });
 }
 
-loadQuiz();
-
 submitButton.addEventListener('click', () => {
+    //scoreQuiz()
+    let incorrectAnswers = []
     let score = 0;
     quizData.forEach((currentQuestion, index) => {
         const answer = document.querySelector(`input[name="answer${index}"]:checked`);
-        if (answer && answer.value === currentQuestion.correct) {
-            score++;
+        if (answer) { 
+            if(answer.value === currentQuestion.correct) {
+                score++;
+            }
+            else{
+               incorrectAnswers.push(`<strong>Question ${index + 1}:</strong> Your Answer: <u>${currentQuestion[answer.value]}</u>. Correct answer: <u>${currentQuestion[currentQuestion.correct]}</u>`) 
+            }
+        }
+        else{
+            incorrectAnswers.push(`<strong>Question ${index + 1}:</strong> Your Answer: <u>None</u>. Correct answer: <u>${currentQuestion[currentQuestion.correct]}</u>`)
         }
     });
-    results.innerHTML = `You scored ${score} out of ${quizData.length}`;
+    let encouragement = ''
+    let percentCorrect = score/quizData.length    
+    if(percentCorrect >= 1) encouragement = 'Yay, you got full marks. Well done!';
+    else if(percentCorrect >= 0.50) encouragement = 'Half way there, keep practising!';
+    else if(percentCorrect >= 0.75) encouragement = 'Almost there, keep at it!';
+    else if(percentCorrect >= 0.90) encouragement = 'So close, just a few more tries!';   
+    else encouragement = 'Keep practising, you get there in no time!';
+    results.innerHTML = `<div><label>You scored ${score} out of ${quizData.length}. ${encouragement}</label></div>`;
+    if(incorrectAnswers.length >0){
+        results.innerHTML+=`<br><div><label>Incorrect Answers:</label><label>`;
+        incorrectAnswers.forEach((incorrectAnswer, index) => {
+            results.innerHTML+= `<label>${incorrectAnswer}</label><br>`
+        });
+        results.innerHTML+='</div>';
+    }
+    
     submitButton.disabled = true;
+    resetButton.disabled = false;
 });
+
+
+resetButton.addEventListener('click', () => { resetQuiz()});
+
+resetQuiz()
