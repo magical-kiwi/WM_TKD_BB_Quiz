7d474f04f81a4b1802f783627b6506bbeeacf5df
Liam Brady <we.are.both.cats@gmail.com>
2025-01-13 18:34:14 +0000

    Fix python path for start server bat file.

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting Python HTTP server on port 8080...
 start "" "http://localhost:8080"  // Open the browser immediately
-"C:\Program Files\Python311\python.exe" -m http.server 8080
+python -m http.server 8080
 pause
