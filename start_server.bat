c8ff279ded9a8382d96de4659b903229c1e1d022
liam.brady <liam.brady@wushustudios.com>
2025-01-14 11:01:35 +0000

    Update the directory for the server to start in.

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Python HTTP server on port 8080...
-start "" "http://localhost:8080"  // Open the browser immediately
-cd release
+start "" "http://localhost:8080/"  // Open the browser immediately
+cd docs
 python -m http.server 8080
 pause
